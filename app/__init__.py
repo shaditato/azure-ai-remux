1b0ebd5ac99e36cef136145578f93026cc7c7782
shaditato <79299258+shaditato@users.noreply.github.com>
2024-01-20 23:26:18 -0500

    Add submit/cancel buttons

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLineEdit, QFormLayout, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QDialogButtonBox, QFormLayout, QLineEdit, QStyle, QWidget
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        # Configure window
         self.setWindowTitle("Azure AI Video Indexer Remux GUI")
         self.resize(500, 150)
         # Create layout
@@ -14,8 +15,24 @@ class MainWindow(QWidget):
         layout.addRow("API Key", api_key)
         layout.addRow("Account ID", QLineEdit())
         layout.addRow("Video URL", QLineEdit())
-        layout.addRow(QPushButton("Send to Azure AI Video Indexer"))
+        # Add submit/cancel buttons
+        button_box = QDialogButtonBox()
+        accept_btn = button_box.addButton("Send to Azure AI Video Indexer", QDialogButtonBox.ButtonRole.AcceptRole)
+        accept_icon = self.style().standardIcon(QStyle.SP_DialogApplyButton)
+        accept_btn.setIcon(accept_icon)
+        button_box.addButton(QDialogButtonBox.StandardButton.Cancel)
+        button_box.clicked.connect(lambda btn: self.handleButtonClick(btn, button_box))
+        layout.addRow(button_box)
+        # Set layout
         self.setLayout(layout)
+    
+    def handleButtonClick(self, button, button_box):
+        role = button_box.buttonRole(button)
+        if role == QDialogButtonBox.ButtonRole.AcceptRole:
+            print("applied")
+            # link Azure functions
+        elif role == QDialogButtonBox.ButtonRole.RejectRole:
+            self.close()
 
 def main():
     app = QApplication(sys.argv)
